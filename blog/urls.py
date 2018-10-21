f0b995f8f09aabdfa429772d32238267238a001e
Lucas Lima <llldc21@gmail.com>
2018-10-21 17:54:26 -0300

    Adicionando paginas especificas de cada post

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
